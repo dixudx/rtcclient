44751be7ece9766c0bfd497979d16f15eb943972
stephenhsu <stephenhsu90@gmail.com>
2015-07-20 23:03:43 +0800

    add method for single role and member

--- rtcclient/project_area.py
+++ rtcclient/project_area.py
@@ -25,6 +25,11 @@ class ProjectArea(RTCBase):
         return self.rtc_obj
 
     def initialize(self, data):
+        """
+        initialize the object from the response xml data
+        :param data: xml data
+        :return:
+        """
         self.log.debug("Start initializing data from %s",
                        self.url)
         self._initialize(data)
@@ -33,6 +38,12 @@ class ProjectArea(RTCBase):
                       self)
 
     def getRoles(self):
+        """
+        get all roles in this project area
+        :return: Roles object list
+        """
+        self.log.info("Get all the roles in <ProjectArea %s>",
+                      self)
         resp = requests.get(self.roles_url,
                             verify=False,
                             headers=self.rtc_obj.headers)
@@ -41,8 +52,8 @@ class ProjectArea(RTCBase):
         raw_data = xmltodict.parse(resp.content)
         roles_raw = raw_data['jp06:roles']['jp06:role']
         if not roles_raw:
-            self.log.warning("No roles found in this ProjectArea:<%s>",
-                             self.name)
+            self.log.warning("There are no roles in this <ProjectArea %s>",
+                             self)
             return None
         for role_raw in roles_raw:
             role = Role(role_raw.get("jp06:url"), self.rtc_obj)
@@ -50,7 +61,30 @@ class ProjectArea(RTCBase):
             roles_list.append(role)
         return roles_list
 
+    def getRole(self, label):
+        """
+        get the role object by the label name
+        :param label: the role label name
+        :return: Role object
+        """
+        roles = self.getRoles()
+        for role in roles:
+            if role.label == label:
+                self.log.info("Get <Role %s> in this <ProjectArea %s>",
+                              role, self)
+                return role
+        else:
+            self.log.error("No role's label is %s in this <ProjectArea %s>",
+                           role, self)
+            return None
+
     def getMembers(self):
+        """
+        get all the members in this project area
+        :return: Members object list
+        """
+        self.log.info("Get all the members in <ProjectArea %s>",
+                      self)
         resp = requests.get(self.members_url,
                             verify=False,
                             headers=self.rtc_obj.headers)
@@ -59,7 +93,7 @@ class ProjectArea(RTCBase):
         raw_data = xmltodict.parse(resp.content)
         members_raw = raw_data['jp06:members']['jp06:member']
         if not members_raw:
-            self.log.warning("No member found in this ProjectArea:<%s>",
+            self.log.warning("There are no members in this ProjectArea:<%s>",
                              self.name)
             return None
         for member_raw in members_raw:
@@ -67,6 +101,23 @@ class ProjectArea(RTCBase):
             member.initialize(member_raw)
             members_list.append(member)
         return members_list
+
+    def getMember(self, email):
+        """
+        get the member object by the email address
+        :param email: the email addr (e.g. somebody@gmail.com)
+        :return: the member object
+        """
+        members = self.getMembers()
+        for member in members:
+            if member.email == email:
+                self.log.info("Get <Member %s> in this <ProjectArea %s>",
+                              member, self)
+                return member
+        else:
+            self.log.error("No member's email is %s in this <ProjectArea %s>",
+                           member, self)
+            return None
 
 
 class Role(RTCBase):
