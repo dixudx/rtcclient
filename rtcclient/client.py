ce4a6d600980c0b1c4202b2af6a8070aaad30822
gpongelli <gpongelli@users.noreply.github.com>
2023-04-06 12:39:00 +0200

    propagate flag to _handle_resource_entry

--- rtcclient/client.py
+++ rtcclient/client.py
@@ -1514,7 +1514,8 @@ class RTCClient(RTCBase):
                                                        entries,
                                                        projectarea_url=pa_url,
                                                        archived=archived,
-                                                       filter_rule=filter_rule)
+                                                       filter_rule=filter_rule,
+                                                       skip_full_attributes = skip_full_attributes)
                 if resource is not None:
                     resources_list.append(resource)
                 break
@@ -1527,7 +1528,7 @@ class RTCClient(RTCBase):
                             None,
                             p.starmap(self._handle_resource_entry,
                                       [(resource_name, entry, pa_url, archived,
-                                        filter_rule) for entry in entries]))))
+                                        filter_rule, skip_full_attributes) for entry in entries]))))
 
             # find the next page
             url_next = raw_data.get('oslc_cm:Collection').get('@oslc_cm:next')
@@ -1555,7 +1556,8 @@ class RTCClient(RTCBase):
                                entry,
                                projectarea_url=None,
                                archived=False,
-                               filter_rule=None):
+                               filter_rule=None,
+                               skip_full_attributes=True):
         """
         :param filter_rule: a list of filter rules
             e.g. filter_rule = [("dc:creator", "@rdf:resource",
@@ -1611,7 +1613,7 @@ class RTCClient(RTCBase):
         else:
             resource_url = entry.get("@rdf:resource")
 
-        resource = resource_cls(resource_url, self, raw_data=entry)
+        resource = resource_cls(resource_url, self, raw_data=entry, skip_full_attributes=skip_full_attributes)
         return resource
 
     def queryWorkitems(self,
